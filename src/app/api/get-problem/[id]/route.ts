1b42ca0277b4094dc5d0f267876f011e909a80b5
Arijit Ganguly <aganguly158@gmail.com>
2025-04-27 01:57:50 +0530

    routing error fixed

--- src/app/api/get-problem/[id]/route.ts
+++ src/app/api/get-problem/[id]/route.ts
@@ -2,42 +2,35 @@ import { NextRequest, NextResponse } from 'next/server';
 
 import problemService from '@/lib/problemServiceInstance';
 
-// Define CORS headers once
-const corsHeaders = {
-  'Access-Control-Allow-Origin': '*',
-  'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
-  'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+const headers = {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS',
+    'Access-Control-Allow-Headers': 'Content-Type, Authorization',
 };
 
-export async function GET(_req: NextRequest, { params }: { params: { id: string } }) {
-  try {
-    const { id } = params;
-    const response = await problemService.getProblem(id);
+export async function GET(_req: NextRequest,  { params }: { params: Promise<{ id: string }>}) {
+    try {
+        const { id } = await params;
+        const response = await problemService.getProblem(id);
 
-    return NextResponse.json(
-      {
-        success: true,
-        message: 'Successfully fetched a problem',
-        data: response,
-        error: {},
-      },
-      {
-        status: 200,
-        headers: corsHeaders,
-      }
-    );
-  } catch (error) {
-    return NextResponse.json(
-      {
-        success: false,
-        message: 'Something went wrong',
-        data: {},
-        error,
-      },
-      {
-        status: 500,
-        headers: corsHeaders,
-      }
-    );
-  }
+        return NextResponse.json({
+            success: true,
+            message: 'Successfully fetched a problem',
+            data: response,
+            error: {}
+        }, { 
+            status: 200,
+            headers
+        });
+    } catch (error) {
+        return NextResponse.json({
+            success: false,
+            message: 'Something went wrong',
+            data: {},
+            error
+        }, { 
+            status: 500,
+            headers
+        });
+    }
 }
