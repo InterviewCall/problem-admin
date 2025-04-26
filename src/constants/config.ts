7b0e51f27acffac04a2ef39ec8c9542a25e96b74
Arijit Ganguly <aganguly158@gmail.com>
2025-04-27 01:30:28 +0530

    siteconfig updated

---
+++ src/constants/config.ts
@@ -0,0 +1,7 @@
+export const siteConfig = {
+    title: 'InterviewCall Problem Admin Portal',
+    description:
+      'Easily add, update, and manage problems with the InterviewCall Problem Admin Portal.',
+    /** Without additional '/' on the end, e.g. https://theodorusclarence.com */
+    url: 'https://www.problem-admin.interviewcall.club',
+};
